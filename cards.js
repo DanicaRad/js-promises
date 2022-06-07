5683be801d68729e7a66b5939685bd69b17832d4
DanicaRad <87402525+DanicaRad@users.noreply.github.com>
2022-06-07 13:51:19 -0700

    added error handling

--- cards.js
+++ cards.js
@@ -3,6 +3,7 @@
 const cardsBaseURL = 'http://deckofcardsapi.com/api/deck'
 const cardBtn = document.getElementById("draw-card")
 const cardsDiv = document.getElementById("cards")
+const errMsg = document.getElementById("error")
 
 let deckId
 
@@ -18,11 +19,16 @@ function getDeck() {
 }
 
 cardBtn.addEventListener("click", () => {
+    errMsg.innerText = ""
     axios.get(`${cardsBaseURL}/${deckId}/draw/?count=1`)
         .then(data => {
             // let img = data.data.cards[0].image
             let card = document.createElement("img")
             card.src = data.data.cards[0].image
             cardsDiv.prepend(card)
+        })
+        .catch(err => {
+            console.log(err)
+            errMsg.innerText = "Something went wrong. Try again."
         })
 })
